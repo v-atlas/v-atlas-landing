0eb3f2f6fc26a15900e8d4327fb39e1339638b05
Emeka Allison <emekaallison4@gmail.com>
2023-12-01 19:49:33 +0100

    Update: Website copy edits

--- app/components/Home/Header.tsx
+++ app/components/Home/Header.tsx
@@ -1,12 +1,14 @@
 const Header = () => {
-  return ( 
-    <header className="h-20 pt-5 bg-black text-antiflash-white">
-      <div className="flex justify-between items-center max-w-6xl mx-auto px-5">
-        <h2 className="font-bold text-3xl italic">V-Atlas</h2>
-        <button className="p-4 bg-honolulu-blue text-antiflash-white rounded-lg">Join the waitlist</button>
+  return (
+    <header className='h-20 pt-5 bg-black text-antiflash-white'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto px-5'>
+        <h2 className='font-bold text-3xl italic'>V-Atlas</h2>
+        <button className='p-4 bg-honolulu-blue text-antiflash-white rounded-lg'>
+          Get Started
+        </button>
       </div>
     </header>
-   );
-}
- 
+  );
+};
+
 export default Header;
