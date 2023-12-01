fc7b24786f6706c0ead7ebe6abd6d9b7a4922a9f
Adaeze Ndupu <andupu4@gmail.com>
2023-12-01 14:44:28 +0100

    refactor: update footer

--- app/components/Home/Footer.tsx
+++ app/components/Home/Footer.tsx
@@ -4,11 +4,11 @@ const Footer = () => {
       <div className='sm:w-[80vw] lg:w-full max-w-6xl mx-auto px-5'>
         <div className='pt-16 pb-12 px-5 md:px-12 bg-black text-antiflash-white rounded-3xl mb-20'>
           <h2 className='mb-7 text-3xl md:text-6xl font-bold'>Join the waitlist</h2>
-          <p className='mb-12 md:text-lg md:w-2/3'>
+          <p className='mb-12 md:text-lg sm:w-2/3 lg:w-2/4'>
             Enter your email address to join our mailing list and be the first
             to know when V-ATLAS is ready
           </p>
-          <div className='p-2 flex flex-col md:flex-row gap-2 md:w-2/3 rounded-lg shadow-lg'>
+          <div className='p-2 flex flex-col md:flex-row gap-2 lg:w-2/3 rounded-lg shadow-lg'>
             <div className='md:w-[70%]'>
               <input
                 className='w-full h-full text-black p-4 md:p-2 bg-antiflash-white border rounded-lg border-honolulu-blue'
@@ -27,7 +27,7 @@ const Footer = () => {
             251 Little Falls Drive, Wilmington, New Castle County, Delaware.
           </div>
           <div>hello@v-atlas.com</div>
-          <div className='flex gap-4'>
+          <div className='flex gap-4 flex-wrap'>
             <div>X</div>
             <div>Instagram</div>
             <div>LinkedIn</div>
